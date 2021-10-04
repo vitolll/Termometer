a543b0e7f1e1563e7e146a10858c275e899fe578
vitolll <vito.aritonang99@gmail.com>
2021-10-04 18:52:19 +0800

    ngumpulin tugas

--- termometer.js
+++ termometer.js
@@ -1,5 +1,35 @@
-function termometer(k){
+function KtoC(k){
     return C = k - 273
 }
 
-console.log(termometer(10));
+console.log(KtoC(10));
+
+function FtoC(f){
+    return C = 5/9 * (f-32)
+}
+
+console.log(FtoC(14));
+
+function CtoF(c){
+    return F = 9/5 * c + 32
+}
+
+console.log(CtoF(10));
+
+function KtoF(k){
+    return F = 9/5 * (k - 273) + 32
+}
+
+console.log(KtoF(13));
+
+function FtoK(f){
+    return K = 5/9 * (f - 32) + 273
+}
+
+console.log(FtoK(23));
+
+function CtoK(c){
+    return k = c + 273
+}
+
+console.log(CtoK(27));
